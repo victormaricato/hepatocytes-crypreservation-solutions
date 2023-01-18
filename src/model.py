80a1c5ad23fba0ae05ffb224f2a1e7fa405eb30d
Victor Maricato <maricatovictor@gmail.com>
2023-01-18 02:42:39 -0300

    feat: add hepatocyte cryopreservation optimization (#1)
    * feat: data ingestion and processing
    
    * feat: feature engineering and selection
    
    * feat: Feature Engineering
    
    * feat: modelling
    
    * feat: evaluation
    
    * feat: genetic algorithm
    
    * feat: shap values
    
    * feat: model selection
    
    * chore: requirements and linting
    
    Co-authored-by: Victor Maricato <victormaricato@cloud.upwork.com>

---
+++ src/model.py
@@ -0,0 +1,44 @@
+# from lazypredict.Supervised import LazyRegressor
+from typing import Literal
+
+from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor
+from sklearn.linear_model import ElasticNet, Lasso, LinearRegression, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+
+models = {
+    "Linear Regression": LinearRegression,
+    "Ridge": Ridge,
+    "Lasso": Lasso,
+    "ElasticNet": ElasticNet,
+    "KNeighborsRegressor": KNeighborsRegressor,
+    "SVR": SVR,
+    "RandomForestRegressor": RandomForestRegressor,
+    "ExtraTreesRegressor": ExtraTreesRegressor,
+    "XGBRegressor": XGBRegressor,
+}
+
+
+class Model:
+    def __init__(
+        self,
+        model_class: Literal[
+            "Linear Regression",
+            "Ridge",
+            "Lasso",
+            "ElasticNet",
+            "KNeighborsRegressor",
+            "SVR",
+            "RandomForestRegressor",
+            "ExtraTreesRegressor",
+            "XGBRegressor",
+        ],
+    ):
+        self.model = models[model_class]()
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.model.predict(X)
